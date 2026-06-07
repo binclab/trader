8084398cd65323203ff05411703a80a9d0ea3043
Bret Joseph <bretjoseph@gmail.com>
2026-06-07 16:51:43 +0200

    update

--- docs/js/main.js
+++ docs/js/main.js
@@ -6,5 +6,5 @@ const params = new URLSearchParams(window.location.search);
 const clientId = params.get("client_id");
 if (clientId) {
     localStorage.setItem("clientId", clientId);
-    localStorage.setItem("hostname", params.get("hostname"));
+    localStorage.setItem("hostname", params.get("redirect_uri"));
 }
